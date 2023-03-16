890797b182cddddd425266f9b6a3032aba81ef1a
deez nuts <ov3rsights@protonmail.com>
2023-03-16 20:30:53 +0000

    base backend render interface and reference

---
+++ main.py
@@ -0,0 +1,32 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    screen.fill("blue")
+
+    pygame.draw.circle(screen, "red", player_pos, 10)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+
+    pygame.display.flip()
+    dt = clock.tick(144) / 1000
